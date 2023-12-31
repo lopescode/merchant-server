e0d552c1116800e6dec69e5b13d466ecb1377426
wayTo <yurigarciadev@gmail.com>
2023-12-31 17:05:23 -0300

    aa

--- src/infra/database/prisma/migrations/20231231181520_blueprintingame_indexingame/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `indexInGame` to the `BlueprintInGame` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `blueprintingame` ADD COLUMN `indexInGame` INTEGER NOT NULL;
